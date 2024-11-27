6ecb7a53a753d34e21d66a0a90a6125df91ca9c7
Vladislav Baskakov <bla-bla@bla.com>
2024-11-27 14:14:21 +0300

    Исправил скрипт

--- lesson1.sh
+++ lesson1.sh
@@ -9,15 +9,4 @@ else
     echo "Привет, $user_name!"
 fi
 
-for i in {1..5}; do
-    echo "Это итерация номер $i"
-    done
 
-
-function add_numbers() {
-    local sum=$(( \$1 + \$2 ))  # Используем \$1 и \$2 для аргументов функции
-    echo "Сумма \$1 и \$2 равна $sum"
-}
-
-
-add_numbers 5 10
